2da4c246f8e9c4e407d6f500ab610b2dcc611265
jagadeeswarS <j.somala3@gmail.com>
2025-10-30 02:59:04 +0530

    misc commit

---
+++ include/resource-lite.h
@@ -0,0 +1,17 @@
+#ifndef __RES_LITE__
+#define __RES_LITE__
+
+
+namespace resman_lite
+{
+    class resman
+    {
+    public:
+        
+
+    protected:
+        
+    };
+}
+
+#endif // __RES_LITE__
---
+++ src/resman-lite.cpp
@@ -0,0 +1,7 @@
+#include "resource-lite.h"
+#include "cxxopts.hpp"
+
+int main(int argc, char **argv)
+{
+    cxxopts::Options objOptions("resman-lite", "Cross-Platform resource bundler based on llvm");
+}
